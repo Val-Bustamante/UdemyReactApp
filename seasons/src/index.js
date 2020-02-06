64b7b6550832b46f9288e1fb3d2e4c9c2bcaeb3e
Valentina Bustamante <valentinabustamante@Mobiledevs-MacBook-Pro.local>
2020-02-06 11:30:50 -0500

    Error message added when no geolocation found

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -9,24 +9,33 @@ class App extends React.Component {
 
     //according to js, this is necessary
     constructor(props) {
+
         //since the Component class is being overwritten, we must refrence its contsructor function using super
         super(props);
         this.state = {
-            latitude: null
+            latitude: null,
+            errorMessage: null
         };
+
+        //find user's current location
+        window.navigator.geolocation.getCurrentPosition(
+            //Argument 1: success callback
+            (position) => {
+                this.setState({ latitude: position.coords.latitude });
+            },
+            //Argument 2: failure callback
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        )
     }
 
     //according to react, render must always be defined
     render() {
-        //find user's current location
-        window.navigator.geolocation.getCurrentPosition(
-            //Argument 1: success callback
-            (position) => console.log(position),
-            //Argument 2: failure callback
-            (err) => console.log(err)
-
+        return (
+            <div> Latitude: {this.state.latitude} <br />
+                Error: {this.state.errorMessage}</div>
         )
-        return <div> Latitude: </div>
     }
 }
 
