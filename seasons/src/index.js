5be6596b589ba8ad3f247fb69f1fbd1c707549b6
Valentina Bustamante <valentinabustamante@Mobiledevs-MacBook-Pro.local>
2020-02-06 10:52:13 -0500

    Created App class

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,20 +4,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    //find user's current location
-    window.navigator.geolocation.getCurrentPosition(
-        //Argument 1: success callback
-        (position) => console.log(position),
-        //Argument 2: failure callback
-        (err) => console.log(err)
+//subclassing React.Component to utilize its functionality
+class App extends React.Component {
 
-    )
-    return (
-        <div>
-            Hi there!
-        </div>
-    )
+    //according to js, this is necessary
+    constructor(props) {
+        //since the Component class is being overwritten, we must refrence its contsructor function using super
+        super(props);
+        this.state = {
+            latitude: null
+        };
+    }
+
+    //according to react, render must always be defined
+    render() {
+        //find user's current location
+        window.navigator.geolocation.getCurrentPosition(
+            //Argument 1: success callback
+            (position) => console.log(position),
+            //Argument 2: failure callback
+            (err) => console.log(err)
+
+        )
+        return <div> Latitude: </div>
+    }
 }
 
 ReactDOM.render(
