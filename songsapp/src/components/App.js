c06eb9197198cdf27e341a78abe20459b09d9643
Valentina Bustamante <valentinabustamante@Mobiledevs-MacBook-Pro.local>
2020-02-11 13:31:27 -0500

    Beginning set up

---
+++ songsapp/src/components/App.js
@@ -0,0 +1,9 @@
+//not a class based component because we're not making use of state
+import React from 'react'
+
+//functional component because we're storing data inside of redux instead of state within react components
+const App = () => {
+    return <div>App</div>
+};
+
+export default App;
